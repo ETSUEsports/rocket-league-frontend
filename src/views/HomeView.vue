257b606cfad0eeee8b9b45e90c88128f83a3d27c
Russell Payne <ryois@ryois.me>
2023-01-21 03:16:11 -0500

    Redo Vue frontend

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,9 @@
-<template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/E_LOGO_RGB.png">
-    <LandingPage msg="Welcome to ETSU Esports Rocket League Overlay" />
-  </div>
-</template>
-
-<script>
-import LandingPage from '@/components/LandingPage.vue'
-
-export default {
-  name: 'HomeView',
-  components: {
-    LandingPage
-  }
-}
+<script setup lang="ts">
+import TheWelcome from '../components/TheWelcome.vue'
 </script>
+
+<template>
+  <main>
+    <TheWelcome />
+  </main>
+</template>
