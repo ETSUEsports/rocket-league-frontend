86c4a50f0aeb3584c5183634acd2f7ad6bf871ce
Russell Payne <ryois@ryois.me>
2023-02-12 21:10:53 -0500

    Add return

--- src/components/connectors/OBS.js
+++ src/components/connectors/OBS.js
@@ -4,11 +4,12 @@ const toast = useToast();
 function detectOBS(){
     if(!window.obsstudio){
         console.log("OBS not detected");
-        return;
+        return false;
     } else {
         console.log("OBS detected");
         console.log(window.obsstudio.pluginVersion);
         toast.info("OBS detected", { timeout: 2000 });
+        return true;
     }
 }
 
